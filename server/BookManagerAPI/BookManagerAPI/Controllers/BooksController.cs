d78c2f49def3488acb44f1d01c9674d41736d897
T.P.G.D. Prilakshana <deshanipalliyaguruge2000@gmail.com>
2024-12-29 20:44:35 +0530

    first commit

---
+++ server/BookManagerAPI/BookManagerAPI/Controllers/BooksController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BookManagerAPI.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BookManagerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private static List<Book> books = new List<Book>();
+        private static int nextId = 1;
+
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            return Ok(books);
+        }
+
+        [HttpPost]
+        public IActionResult AddBook([FromBody] Book book)
+        {
+            book.Id = nextId++;
+            books.Add(book);
+            return CreatedAtAction(nameof(GetBookById), new { id = book.Id }, book);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBookById(int id)
+        {
+            var book = books.FirstOrDefault(b => b.Id == id);
+            if (book == null) return NotFound();
+            return Ok(book);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, [FromBody] Book updatedBook)
+        {
+            var book = books.FirstOrDefault(b => b.Id == id);
+            if (book == null) return NotFound();
+
+            book.Title = updatedBook.Title;
+            book.Author = updatedBook.Author;
+            book.ISBN = updatedBook.ISBN;
+            book.PublicationDate = updatedBook.PublicationDate;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            var book = books.FirstOrDefault(b => b.Id == id);
+            if (book == null) return NotFound();
+
+            books.Remove(book);
+            return NoContent();
+        }
+    }
+}
+
